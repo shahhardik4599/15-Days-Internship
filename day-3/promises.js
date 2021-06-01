21432800d8e762a4f8869da0d9298eec4eb32207
shahhardik4599 <hardikshah_2000@yahoo.com>
2021-06-01 11:33:52 +0530

    promises in nodejs

---
+++ day-3/promises.js
@@ -0,0 +1,18 @@
+var mypromis = new Promise(function(resolve , reject){
+    const x = 40;
+    const y = 40;
+    if(x===y){
+        resolve();
+    }
+
+    else{
+        reject();
+    }
+})
+mypromis.
+    then(function(){
+        console.log('success');
+    }).
+    catch(function(){
+        console.log('error');
+    });
