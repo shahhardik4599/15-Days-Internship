f43a255f0b056c4c2e3bd8402546b418e37fd151
shahhardik4599 <hardikshah_2000@yahoo.com>
2021-06-04 14:42:29 +0530

    making feedback form and fetching and displaying data on site

--- day-5/expressintern/routes/index.js
+++ day-5/expressintern/routes/index.js
@@ -18,5 +18,19 @@ router.get('/contact', function(req, res, next) {
   res.render('contact', { title: 'Express' });
 });
 
+router.get('/form', function(req, res, next) {
+  res.render('form', { title: 'Express' });
+});
+
+router.post('/form-process', function(req, res, next) {
+  var r1 = parseInt(req.body.num1);
+  var r2 = parseInt(req.body.num2);
+  var name = req.body.name;
+  var total = r1+r2;
+  console.log(req.body);
+  res.render('feedback',{rt1:r1,rt2:r2,name:name,total:total});
+});
+
+
 
 module.exports = router;
