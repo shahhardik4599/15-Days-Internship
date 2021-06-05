0f77a8b9cac9a1271a1ef1ff7dcbf63a6886d5e6
shahhardik4599 <hardikshah_2000@yahoo.com>
2021-06-05 18:37:33 +0530

    connecting with database

--- day-5/expressintern/routes/index.js
+++ day-5/expressintern/routes/index.js
@@ -1,6 +1,21 @@
 var express = require('express');
+var mysql      = require('mysql');
 var router = express.Router();
 
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'hardik_cafe'
+});
+connection.connect(function(err){
+  if(!err){
+    console.log("DB connected");
+  }
+  else{
+    console.log("DB connection issue !");
+  }
+});
 /* GET home page. */
 // router.get('/', function(req, res, next) {
 //   res.render('index', { title: 'Express' });
@@ -22,15 +37,39 @@ router.get('/form', function(req, res, next) {
   res.render('form', { title: 'Express' });
 });
 
-router.post('/form-process', function(req, res, next) {
-  var r1 = parseInt(req.body.num1);
-  var r2 = parseInt(req.body.num2);
-  var name = req.body.name;
-  var total = r1+r2;
+// router.post('/feedback', function(req, res, next) {
+//   var r1 = parseInt(req.body.num1);
+//   var r2 = parseInt(req.body.num2);
+//   var name = req.body.name;
+//   var total = r1+r2;
+//   var mob = parseInt(req.body.num);
+//   var suggest = req.body.suggest;
+
+//   console.log(req.body);
+//   res.render('feedback',{rt1:r1,rt2:r2,name:name,total:total,mob:mob,suggest,suggest});
+// });
+
+router.post('/add-process', function(req, res, next) {
   console.log(req.body);
-  res.render('feedback',{rt1:r1,rt2:r2,name:name,total:total});
+  const mybodydata={
+    Name : req.body.name,
+    Mob : req.body.num,
+    rt1 : req.body.num1,
+    rt2: req.body.num2,
+    suggest: req.body.suggest
+  }
+  connection.query("insert into user_feedback set ?",mybodydata,function(err,result){
+    if(err) throw err;
+    res.redirect('form');
+  });
 });
 
-
+  router.get('/feedback', function(req, res, next) {
+    connection.query("select * from user_feedback",function(err,db_rows){
+      if(err) throw err;
+      console.log(db_rows);
+      res.render('feedback',{db_rows : db_rows});
+    });
+});
 
 module.exports = router;
